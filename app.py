7f7a8a5027202c2a05d6cc709d07c0b5a476b2a0
sgershuny <72019925+sgershuny@users.noreply.github.com>
2020-11-30 17:05:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,9 @@ app = Flask(__name__)
 
 # Note: http:// or https:// must be the prefix for proper redirection
 websites = [
-        "https://goo.gl/forms/FWuOP1Ytw8T0cM3a2",
-        "https://goo.gl/forms/aBfYH3CsoonSzBUn1",
-        "https://goo.gl/forms/AKWAOvOFRmjSUJAV2",
-        "https://goo.gl/forms/DsFBq1C4cSELvLTj1",
-        "https://goo.gl/forms/sU1CZDEjjfsTiBsz1",
-        "https://goo.gl/forms/7Kba0aGql39nL2jg2",
-        "https://goo.gl/forms/HFs8VPinD7gA3HWF3",
-        "https://goo.gl/forms/NZeJPoVcfgO5Xk903"
+        "https://www.bbc.co.uk/sport/football",
+        "google.com",
+        "apple.com"
     ]
 
 @app.route('/')
